60e04ce75eed7dc43a660b28ef0f32500fcb12af
unknown <ik012982i10@206-09-hz109060.H012982.NET>
2014-09-17 12:09:44 +0200

    uno

---
+++ Tabla/tabla.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+<?php 
+  echo'<table border=1>'; 
+  for ($x=0; $x<=$_GET["name"]; $x++) {
+    echo'<table><tr>
+    <td>HOLA</td>
+    <td>HOLA</td>
+    </tr>
+    </table>';
+  }
+?>
+</body>
+</html>
